5dde6a1db6f1cb7c8ce54ad7ffedd13e2012ee2a
pdellis85 <pdellis85@gmail.com>
2020-07-25 20:14:22 -0500

    commit

--- _python/python_fundamentals/fun_bas2.py
+++ _python/python_fundamentals/fun_bas2.py
@@ -1,12 +1,32 @@
-#1 Countdown
+# 1 Countdown
 
-num=input('Enter number here: ')
+a = input('input your number here: ')
 
-def countDown(num):
-  for x in range(int(num),-1,-1):
-    print(x)
 
-countDown(num)
+def countDown(a):
+    numList = []
+    for val in range(int(a), -1, -1):
+        numList.append(val)
+    return numList
 
-#2 Print and Return
 
+print(countDown(a))
+
+# 2 Print and Return
+
+def printReturn(NList):
+
+    print(NList[0])
+    return (NList[1])
+
+print(printReturn([1, 2]))
+
+# 3 First Plus Length
+
+def length(nums_list):
+    x = (nums_list[0])
+    y = len(nums_list)
+    z = x + y
+    return z
+
+print(length([1,2,3,4,5]))
