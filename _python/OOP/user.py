3be09c06ef6e1b67463c9b38ef3d321fb1f9053d
pdellis85 <pdellis85@gmail.com>
2020-08-03 17:28:17 -0500

    commit

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -1,53 +1,46 @@
-class User:		# here's what we have so far
+class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
         self.account_balance = 0
-    # adding the deposit method
 
-    def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
-        # the specific user's account increases by the amount of the value received
+    def make_deposit(self, amount):
         self.account_balance += amount
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
 
     def display_user_balance(self):
-        print(f"User: {self.name}, Balance: ${self.account_balance}")
+        print("User: " + self.name, "Balance: $" + str(self.account_balance))
 
     def transfer_money(self, other_user, amount):
-        # BONUS
-        # let's leverage the fact that we have deposit and withdrawal methods that are available to self and other_user
-        # since they're both User objects...You don't have to do it this way though
-        # could also say other_user.account_balance += amount
         other_user.make_deposit(amount)
-        # could also say self.account_balance -= amount
         self.make_withdrawal(amount)
 
 
-todd = User("Todd", "todd@todd.com")
-jane = User("Jane", "jane@jane.com")
-john = User("John", "john@john.com")
+rash = User("Rashaud", "rash@rash.com")
+porshea = User("Porshea", "porsh@porsh.com")
+kathy = User("Kathy", "kat@kat.com")
 
-todd.make_deposit(100)
-todd.make_deposit(100)
-todd.make_deposit(100)
-todd.make_withdrawal(50)
-todd.display_user_balance()
+porshea.make_deposit(100)
+porshea.make_deposit(100)
+porshea.make_deposit(100)
+porshea.make_withdrawal(50)
+porshea.display_user_balance()
 
-jane.make_deposit(50)
-jane.make_deposit(1000)
-jane.make_withdrawal(500)
-jane.make_withdrawal(100)
-jane.display_user_balance()
+rash.make_deposit(50)
+rash.make_deposit(1000)
+rash.make_withdrawal(500)
+rash.make_withdrawal(100)
+rash.display_user_balance()
 
-john.make_deposit(100)
-john.make_withdrawal(25)
-john.make_withdrawal(25)
-john.make_withdrawal(25)
-john.display_user_balance()
+kathy.make_deposit(100)
+kathy.make_withdrawal(25)
+kathy.make_withdrawal(25)
+kathy.make_withdrawal(25)
+kathy.display_user_balance()
 
 # BONUS
-todd.transfer_money(jane, 250)
-todd.display_user_balance()
-jane.display_user_balance()
+rash.transfer_money(kathy, 250)
+rash.display_user_balance()
+kathy.display_user_balance()
