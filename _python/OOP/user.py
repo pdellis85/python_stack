da0c27f222be62734a53d6e5ef63d41c0cdaf9ec
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-05 18:55:46 -0500

    commit

--- _python/OOP/user.py
+++ _python/OOP/user.py
@@ -1,46 +1,26 @@
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, state):
         self.name = name
-        self.email = email
-        self.account_balance = 0
+        self.state = state
+        self.account_balance = 500
 
-    def make_deposit(self, amount):
-        self.account_balance += amount
-
-    def make_withdrawal(self, amount):
+    def make_withdraw(self, amount):
         self.account_balance -= amount
 
     def display_user_balance(self):
-        print("User: " + self.name, "Balance: $" + str(self.account_balance))
-
-    def transfer_money(self, other_user, amount):
-        other_user.make_deposit(amount)
-        self.make_withdrawal(amount)
+        print("User: " + self.name,  "has a Balance of $" +
+              str(self.account_balance))
 
 
-rash = User("Rashaud", "rash@rash.com")
-porshea = User("Porshea", "porsh@porsh.com")
-kathy = User("Kathy", "kat@kat.com")
+rash = User("Rashaud", "Texas")
+porsh = User("Porshea", "Washington")
+kat = User("Kathy", "California")
 
-porshea.make_deposit(100)
-porshea.make_deposit(100)
-porshea.make_deposit(100)
-porshea.make_withdrawal(50)
-porshea.display_user_balance()
+porsh.make_withdraw(20)
+porsh.display_user_balance()
 
-rash.make_deposit(50)
-rash.make_deposit(1000)
-rash.make_withdrawal(500)
-rash.make_withdrawal(100)
+rash.make_withdraw(120)
 rash.display_user_balance()
 
-kathy.make_deposit(100)
-kathy.make_withdrawal(25)
-kathy.make_withdrawal(25)
-kathy.make_withdrawal(25)
-kathy.display_user_balance()
-
-# BONUS
-rash.transfer_money(kathy, 250)
-rash.display_user_balance()
-kathy.display_user_balance()
+kat.make_withdraw(220)
+kat.display_user_balance()
