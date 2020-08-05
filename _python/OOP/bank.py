134768bba319ccca1f00f5f34b5c5df3541a886f
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-05 09:34:19 -0500

    commit

--- _python/OOP/bank.py
+++ _python/OOP/bank.py
@@ -1,18 +1,18 @@
 class BankAccount:
     def __init__(self, int_rate=0, balance=0):
-        self.int_rate = int_rate 
-        self.balance = balance 
-        
+        self.int_rate = int_rate
+        self.balance = balance
+
     def deposit(self, amount):
-        self.balance += amount 
+        self.balance += amount
         return self
 
     def withdraw(self, amount):
-        # check the balance and see if it's greater than the amount 
+        # check the balance and see if it's greater than the amount
         if self.balance >= amount:
-            self.balance -= amount 
+            self.balance -= amount
         else:
-            self.balance -= 5 
+            self.balance -= 5
             print("Insufficient Funds: Charging a $5 fee")
         return self
 
@@ -29,5 +29,7 @@ class BankAccount:
 ba1 = BankAccount(balance=100)
 ba2 = BankAccount(.01, 500)
 
-ba1.deposit(100).deposit(50).deposit(50).withdraw(100).yield_interest().display_account_info()
-ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(50).withdraw(50).yield_interest().display_account_info()
+ba1.deposit(100).deposit(50).deposit(50).withdraw(
+    100).yield_interest().display_account_info()
+ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(
+    50).withdraw(50).yield_interest().display_account_info()
