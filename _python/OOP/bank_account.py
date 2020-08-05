b542d99d983aa6aaf360e330e1025db2102cffb3
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-05 16:33:06 -0500

    commit

--- _python/OOP/bank_account.py
+++ _python/OOP/bank_account.py
@@ -1,49 +1,63 @@
+# make_withdrawal(self, amount) - have this method decrease the user's balance by the amount specified
+# display_user_balance(self) - have this method print the user's name and account balance to the terminal
+# eg. "User: Guido van Rossum, Balance: $150
+# BONUS: transfer_money(self, other_user, amount) - have this method decrease the user's balance by the amount and add that amount to other other_user's balance
 class BankAccount:
-    def __init__(self, int_rate=0, balance=0):
-        self.int_rate = int_rate 
-        self.balance = balance 
-        
+    def __init__(self, int_rate, balance, b_type):
+        self.account_balance = balance
+        self.int_rate = int_rate
+        self.type = b_type
     def deposit(self, amount):
-        self.balance += amount 
+        self.account_balance += amount
         return self
-
     def withdraw(self, amount):
-        # check the balance and see if it's greater than the amount 
-        if self.balance >= amount:
-            self.balance -= amount 
-        else:
-            self.balance -= 5 
-            print("Insufficient Funds: Charging a $5 fee")
+        if amount > self.account_balance:
+            print(f"You are withdrawing {amount} but only have {self.account_balance}, charging $5 fee.")
+            self.account_balance -= (amount+5)
+        self.account_balance -= amount
         return self
-
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"Current balance in {self.type}s: {self.account_balance}")
+        return self
+    def yield_interest(self):
+        amount_earned = self.account_balance * self.int_rate
+        self.account_balance += amount_earned
         return self
 
-    def yield_interest(self):
-        if self.balance > 0:
-            self.balance = self.balance + self.balance * self.int_rate
-        return self
 
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, int_rate, balance):
         self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.accounts = {'checking':BankAccount(int_rate, balance, 'checking')}
+    def create_account(self, int_rate, balance, b_type):
+        self.accounts[b_type] = BankAccount(int_rate, balance, b_type)
+        return self
+    # def make_withdrawal(self, amount):
+    #     self.account.account_balance -= amount
+    #     return amount
+    # def display_user_balance(self):
+    #     print(f"{self.name} has ${self.account.account_balance} remaining.")
+    #     return self
+    # def transfer_money(self, other_user, amount):
+    #     other_user.account.account_balance += self.make_withdrawal(amount)
+    #     print(f"{other_user.name} received ${amount} from {self.name}")
+    #     return self
 
-    class User:
-    def example_method(self):
-        self.account.deposit(100)		# we can call the BankAccount instance's methods
-    	print(self.account.balance)		# or access its attributes
+sean = User("Sean", .02, 1000)
+liz = User("Elizabeth", .05, 500)
+liz.create_account(.04, 200, 'saving')
+liz.accounts['checking'].display_account_info()
+liz.accounts['saving'].display_account_info()
 
-rash = User("Rashaud", "rash@rash.com")
-porshea = User("Porshea", "porsh@porsh.com")
-kathy = User("Kathy", "kat@kat.com")
+# sean.account_balance += 1000
 
+# sean.transfer_money(liz, 250).display_user_balance()
 
+# deposit(self, amount) - increases the account balance by the given amount
+# withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+# display_account_info(self) - print to the console: eg. "Balance: $100"
+# yield_interest(self) - increases the account balance by the current balance * the interest rate (as long as the balance is positive)
 
-ba1 = BankAccount(balance=100)
-ba2 = BankAccount(.01, 500)
+# acct = BankAccount(.02, 100)
 
-ba1.deposit(100).deposit(50).deposit(50).withdraw(100).yield_interest().display_account_info()
-ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(50).withdraw(50).yield_interest().display_account_info()
+# acct.deposit(100).display_account_info().yield_interest().withdraw(50).display_account_info()
