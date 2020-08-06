415013d658b7ba90e9a4e3ad58cf7eb1672f78a6
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-05 21:55:18 -0500

    commit

--- _python/OOP/bank.py
+++ _python/OOP/bank.py
@@ -1,14 +1,13 @@
 class BankAccount:
-    def __init__(self, int_rate=0, balance=0):
-        self.int_rate = int_rate
+    def __init__(self, rate, balance):
+        self.rate = rate
         self.balance = balance
 
-    def deposit(self, amount):
+    def make_deposit(self, amount):
         self.balance += amount
         return self
 
-    def withdraw(self, amount):
-        # check the balance and see if it's greater than the amount
+    def make_withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
         else:
@@ -17,19 +16,19 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print("Balance: $", + self.balance)
         return self
 
     def yield_interest(self):
         if self.balance > 0:
-            self.balance = self.balance + self.balance * self.int_rate
+            self.balance = self.balance + self.balance * self.rate
         return self
 
 
-ba1 = BankAccount(balance=100)
-ba2 = BankAccount(.01, 500)
+ba1 = BankAccount(.40, 500)
+ba2 = BankAccount(.10, 400)
 
-ba1.deposit(100).deposit(50).deposit(50).withdraw(
+ba1.make_deposit(40).make_deposit(80).make_deposit(
     100).yield_interest().display_account_info()
-ba2.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(
-    50).withdraw(50).yield_interest().display_account_info()
+ba2.make_deposit(100).make_deposit(100).make_withdraw(
+    20).make_withdraw(400).yield_interest().display_account_info()
