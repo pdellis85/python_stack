ef7b915caa6d576b9f1d84863b0b02516a314ddf
Porshea Ellis <porsheaellis@macbook-pro.attlocal.net>
2020-08-08 14:33:41 -0500

    commit

--- _python/OOP/bank_account.py
+++ _python/OOP/bank_account.py
@@ -1,8 +1,11 @@
+
+
+
 class BankAccount:
-    def __init__(self, rate, balance, a_type):
+    def __init__(self, rate, balance):
         self.rate = rate
         self.account_balance = balance
-        self.a_type = a_type
+        
 
     def make_deposit(self, amount):
         self.account_balance += amount
@@ -17,28 +20,23 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print("Balance: $", + self.account_balance)
+        print("Balance: $" + str(self.account_balance))
         return self
 
     def yield_interest(self):
-        amount_earned = self.account_balance * self.int_rate
+        amount_earned = self.acctount_balance * self.int_rate
         self.account_balance += amount_earned
         return self
 
 
 class User:
-    def __init__(self, name, int_rate, balance):
+    def __init__(self, name):
         self.name = name
-        self.accounts = {'checking': BankAccount(
-            int_rate, balance, 'checking')}
-
-    def create_account(self, int_rate, balance, b_type):
-        self.accounts[b_type] = BankAccount(int_rate, balance, b_type)
-        return self
+        self.accounts = BankAccount(.05,500)
 
 
-rash = User("Rash", .02, 1000)
-porshea = User("Porshea", .05, 500)
-porshea.create_account(.04, 200, 'saving')
-porshea.accounts['checking'].display_account_info()
-porshea.accounts['saving'].display_account_info()
+rash = User("Rash")
+porsh = User("Porshea")
+
+rash.accounts.make_deposit(100).display_account_info()
+porsh.accounts.make_withdraw(800).display_account_info()
