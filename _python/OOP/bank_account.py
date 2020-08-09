0ddfc104feb83eaca280a9a53de88fa43b04d244
Porshea Ellis <porsheaellis@macbook-pro.attlocal.net>
2020-08-08 21:30:48 -0500

    commit

--- _python/OOP/bank_account.py
+++ _python/OOP/bank_account.py
@@ -2,7 +2,6 @@ class BankAccount:
     def __init__(self, rate, balance):
         self.rate = rate
         self.account_balance = balance
-        
 
     def make_deposit(self, amount):
         self.account_balance += amount
@@ -21,19 +20,19 @@ class BankAccount:
         return self
 
     def yield_interest(self):
-        amount_earned = self.acctount_balance * self.int_rate
-        self.account_balance += amount_earned
+        if self.account_balance > 0:
+            self.account_balance = self.account_balance + self.account_balance * self.rate
         return self
 
 
 class User:
     def __init__(self, name):
         self.name = name
-        self.accounts = BankAccount(.05,500)
+        self.accounts = BankAccount(.05, 500)
 
 
 rash = User("Rash")
 porsh = User("Porshea")
 
-rash.accounts.make_deposit(100).display_account_info()
-porsh.accounts.make_withdraw(800).display_account_info()
+rash.accounts.make_deposit(100).yield_interest().display_account_info()
+porsh.accounts.make_withdraw(700).yield_interest().display_account_info()
