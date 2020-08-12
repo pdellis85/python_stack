4777ef1fcd15dbe46ba96482dedac3a4b7acadb2
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-12 12:25:56 -0500

    commit

---
+++ my_enviroments/django2/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/porsheaellis/Desktop/python_stack/my_enviroments/django2/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
