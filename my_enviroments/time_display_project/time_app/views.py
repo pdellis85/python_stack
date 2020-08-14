e86e82e49d25abf86b381ba89c3d4e91a4199186
Porshea Ellis <porsheaellis@MacBook-Pro.local>
2020-08-14 18:28:56 -0500

    commit\

--- my_enviroments/time_display_project/time_app/views.py
+++ my_enviroments/time_display_project/time_app/views.py
@@ -1,5 +1,15 @@
+
 from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    context = {
+        #"date": strftime("%Y-%m-%d"),
+        "date": strftime("%b %d, %Y"),
+        "time": strftime("%I:%M %p", gmtime())
+
+    }
+    return render(request,'index.html', context)
+
+
