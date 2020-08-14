865ccdffb34de73e1db318f41fd6ba765565d71e
Porshea Ellis <porsheaellis@MacBook-Pro.local>
2020-08-14 17:15:53 -0500

    commit

--- my_enviroments/time_display_project/time_display_project/settings.py
+++ my_enviroments/time_display_project/time_display_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'time_app'
+    'time_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
