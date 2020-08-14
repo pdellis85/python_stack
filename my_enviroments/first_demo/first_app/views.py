45e2237fdf2039b473d3364172200d34fc587b7c
Porshea Ellis <porsheaellis@MacBook-Pro.local>
2020-08-14 16:45:57 -0500

    commit

--- my_enviroments/first_demo/first_app/views.py
+++ my_enviroments/first_demo/first_app/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 
 def index(request):
-    return HttpResponse("placeholder to later display a list of all blogs")
+    return render(request,'index.html')
 
 def new(request):
-    return HttpResponse("placeholder to display a new form to create a new blog")
+    return render(request, 'new.html')
 
 def create(request):
     print("you created a blog post")
     return redirect('/')
 
 def show(request, number):
-    return HttpResponse(f"placeholder to display blog number: {number}" )
+    context = {
+        'blog_id': number
+    }
+    return render(request, 'one_blog.html', context)
 
 def edit(request, number):
     return HttpResponse(f"placeholder to edit blog {number}")
