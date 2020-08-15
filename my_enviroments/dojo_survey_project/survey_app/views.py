196b9313ff093a63ba05f7aae501c577076e9bef
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-15 17:38:10 -0500

    commit

--- my_enviroments/dojo_survey_project/survey_app/views.py
+++ my_enviroments/dojo_survey_project/survey_app/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'form.html')
+    return render(request, 'form.html')
+
 
 def result(request):
     return render(request, 'result.html')
+
 
 def create(request):
     request.session['name'] = request.POST['fname']
     request.session['city'] = request.POST['location']
     request.session['lang'] = request.POST['language']
     request.session['comms'] = request.POST['comment']
-    return redirect('/')
-
+    return redirect('/result')
