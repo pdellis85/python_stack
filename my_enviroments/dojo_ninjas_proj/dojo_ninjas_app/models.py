4bc0029158317fb3ecb28477a225f12db4e35481
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-16 23:31:33 -0500

    commit

--- my_enviroments/dojo_ninjas_proj/dojo_ninjas_app/models.py
+++ my_enviroments/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -16,6 +16,6 @@ class Dojo(models.Model):
 class Ninja(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    dojo_id = models.ForeignKey(Dojo, related_name='dojos', on_delete=models.CASCADE)
+    dojo_id = models.ForeignKey(Dojo, related_name='ninjas', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
