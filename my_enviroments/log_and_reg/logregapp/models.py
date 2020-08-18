c60e3376d7e04c9b59e7d278b1641a7bfba752c8
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-18 16:04:09 -0500

    commit

--- my_enviroments/log_and_reg/logregapp/models.py
+++ my_enviroments/log_and_reg/logregapp/models.py
@@ -1,6 +1,31 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+# First Name - required; at least 2 characters; letters only
+# Last Name - required; at least 2 characters; letters only
+# Email - required; valid format
+# Password - required; at least 8 characters; matches password confirmation
+
+
+class UserManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(
+            r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['first_name']) < 2:
+            errors["first_name"] = "Your first name should be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "Your last name should be at least 2 characters"
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address!"
+        if len(postData['password']) < 8:
+            errors["password"] = "Your password should be at least 8 characters"
+        if postData["password"] != postData["confirm_password"]:
+            errors["confirm_password"] = "Password and confirm password do not match"
+        return errors
 
 
 class User(models.Model):
@@ -10,3 +35,4 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- my_enviroments/log_and_reg/logregapp/views.py
+++ my_enviroments/log_and_reg/logregapp/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("You Did.")
+    return render(request, "log.html")
+
+
+def success(request):
+    if 'name' not in request.session:
+        return redirect('/')
+    return render(request, "success.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        errors = User.objects.validator(request.POST)
+        print(errors)
+        if len(errors) > 0:
+            for key, values in errors.items():
+                messages.error(request, values)
+            return redirect('/')
+        new_user = User.objects.create(
+            first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email'], password=request.POST['password'])
+        request.session['name'] = new_user.first_name
+        return redirect('/success')
+    return redirect('/')
+
+
+def login(request):
+    if request.method == "POST":
+        logged_user = User.objects.filter(email=request.POST['email'])
+        if len(logged_user) > 0:
+            logged_user = logged_user[0]
+            if logged_user.password == request.POST['password']:
+                request.session['name'] = logged_user.first_name
+                return redirect('/success')
+    return redirect('/')
+
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
