3986f336ea1ca183b25074e02d46e595598fca3a
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-29 10:45:36 -0500

    commit

--- django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from dojo_ninjas_app.models import Dojo, Ninja
 
 # Create your views here.
+
+
 def root(request):
     context = {
         'dojos': Dojo.objects.all(),
         'ninjas': Ninja.objects.all()
     }
     return render(request, "home.html", context)
+
 
 def create_dojo(request):
     Dojo.objects.create(
@@ -17,10 +20,11 @@ def create_dojo(request):
     )
     return redirect('/')
 
+
 def create_ninja(request):
     Ninja.objects.create(
         first_name=request.POST['first_name'],
         last_name=request.POST['last_name'],
-        dojo_id=request.POST['dojo_id.dojo'],
+        dojo_id=request.POST['dojo.id'],
     )
     return redirect('/')
