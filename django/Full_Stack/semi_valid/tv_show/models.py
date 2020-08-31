66cea3787471a490f87493d3fe7b738453221bd8
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-30 22:49:18 -0500

    commit

---
+++ django/Full_Stack/semi_valid/tv_show/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=45)
+    release_date = models.DateTimeField()
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
