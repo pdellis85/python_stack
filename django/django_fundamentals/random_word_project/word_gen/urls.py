6029a9911bf1f6aad004c647f3bdb78d0fefc66f
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-30 22:17:23 -0500

    commit

--- django/django_fundamentals/random_word_project/word_gen/urls.py
+++ django/django_fundamentals/random_word_project/word_gen/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.root),
+    path('random_word', views.root),
+    path('reset', views.reset)
 ]
--- django/django_fundamentals/random_word_project/word_gen/views.py
+++ django/django_fundamentals/random_word_project/word_gen/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 
+
 def root(request):
-    return render(request, "home.html")
+    if 'count' not in request.session:
+        request.session['count'] = 0
+    request.session['count'] += 1
+    request.session['word'] = get_random_string(length=14)
+    return render(request, 'home.html')
+
+
+def reset(request):
+    request.session.flush()
+    return redirect('/')
