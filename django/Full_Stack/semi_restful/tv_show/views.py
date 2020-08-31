c14dca3965391edf6708abb013b062ab0b790c35
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-30 21:21:55 -0500

    commit

--- django/Full_Stack/semi_restful/tv_show/views.py
+++ django/Full_Stack/semi_restful/tv_show/views.py
@@ -1,7 +1,63 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 
-def root(request):
-    return render(request, "home.html")
+def index(request):
+    context = {
+        'shows': Show.objects.all()
+    }
+    return render(request, 'shows.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    # CREATE THE SHOW
+    Show.objects.create(
+        title=request.POST['title'],
+        network=request.POST['network'],
+        release_date=request.POST['release_date'],
+        description=request.POST['description']
+    )
+    return redirect('/')
+
+
+def edit(request, show_id):
+    one_show = Show.objects.get(id=show_id)
+    context = {
+        'show': one_show
+    }
+    return render(request, 'edit.html', context)
+
+
+def update(request, show_id):
+    # update the show
+    to_update = Show.objects.get(id=show_id)
+    # update the rest of the show info
+    to_update.title = request.POST['title']
+    to_update.release_date = request.POST['release_date']
+    to_update.network = request.POST['network']
+    to_update.description = request.POST['description']
+    to_update.save()
+
+    return redirect('/')
+
+
+def show(request, show_id):
+    # query for one show with show_id
+    one_show = Show.objects.get(id=show_id)
+    context = {
+        'show': one_show
+    }
+    return render(request, 'show.html', context)
+
+
+def delete(request, show_id):
+    # NOTE: Delete one show!
+    to_delete = Show.objects.get(id=show_id)
+    to_delete.delete()
+    return redirect('/')
