27c64c3e525faa688fafe4857ef4188d23714024
Porshea Ellis <porsheaellis@MacBook-Pro.attlocal.net>
2020-08-31 10:38:08 -0500

    commit

--- django/Full_Stack/book_club/book_app/urls.py
+++ django/Full_Stack/book_club/book_app/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('book_app.urls')),
+     path("", views.index),
+    path("register", views.register),
+    path("login", views.login),
+    path("books", views.show_all),
+    path("books/create", views.create_book),
+    path("books/<int:book_id>", views.show_one),
+    path("books/<int:book_id>/update", views.update),
+    path("books/<int:book_id>/delete", views.delete),
+    path("favorite/<int:book_id>", views.favorite),
+    path("unfavorite/<int:book_id>", views.unfavorite),
+    path("logout", views.logout)
 ]
